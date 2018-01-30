8151d6f057e096d457e739056398f199d196422e
DangerRanger7 <kierradachelle@yahoo.com>
2018-01-29 23:07:43 -0600

    Added basic about activity

---
+++ app/src/main/java/kurtandkierra/blackjack/AboutActivity.java
@@ -0,0 +1,17 @@
+package kurtandkierra.blackjack;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kierra on 1/29/2018.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+    }
+}
---
+++ app/src/main/java/kurtandkierra/blackjack/MainActivity.java
@@ -0,0 +1,28 @@
+/*APP created by Kurt Jones and Kierra Dangerfield*/
+package kurtandkierra.blackjack;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.About_button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),
+                        AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
