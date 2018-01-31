0c62313fc375904af7ee5e09c796f1d1b5d38aba
hkurt <hkurtj11@gmail.com>
2018-01-31 10:35:41 -0600

    Random Number for cards

--- app/src/main/java/kurtandkierra/blackjack/PlayActivity.java
+++ app/src/main/java/kurtandkierra/blackjack/PlayActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 /**
  * Created by Kierra on 1/30/2018.
  */
@@ -15,11 +17,17 @@ public class PlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-/*
-        final TextView points_tv = findViewById(R.id.player_points);
-        String points_s = points_tv.getText().toString().trim();
-        final int points_i = Integer.parseInt(points_s);
-*/
+
+        Random ran = new Random();
+        int card_num = ran.nextInt(13)+1;//13 is max num 1 is min
+        /*
+        1=Ace
+        2-10 = 2-10
+        11=Jack
+        12=Queen
+        13=King
+        */
+
         Button b = findViewById(R.id.win_points);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
