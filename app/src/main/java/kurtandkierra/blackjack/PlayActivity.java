26b88441d2e450a8ae515e357d5f2e45d7931529
hkurt <hkurtj11@gmail.com>
2018-02-08 11:03:22 -0600

    added & used card images

--- app/src/main/java/kurtandkierra/blackjack/PlayActivity.java
+++ app/src/main/java/kurtandkierra/blackjack/PlayActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,12 +44,19 @@ public class PlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 TextView textView;
+                ImageView imageView;
 
-                // set unseen cards to 0
+                /*/ set unseen cards to 0
                 textView = findViewById(R.id.player_c3);
                 textView.setText("0");
                 textView = findViewById(R.id.dealer_c2);
-                textView.setText("0");
+                textView.setText("0");//*/
+
+                // set unseen cards to back
+                imageView = findViewById(R.id.player_ci3);
+                imageView.setImageResource(R.drawable.card_back);
+                imageView = findViewById(R.id.dealer_ci2);
+                imageView.setImageResource(R.drawable.card_back);
                 // set totals to 0
                 textView = findViewById(R.id.dealer_total);
                 textView.setText("0");
@@ -160,6 +168,7 @@ public class PlayActivity extends AppCompatActivity {
 
     public void draw_card(int place){
             TextView textView;
+            ImageView imageView;
             Random ran = new Random();
             int card_num = ran.nextInt(13) + 1;//13 is max num 1 is min
             /*
@@ -177,75 +186,64 @@ public class PlayActivity extends AppCompatActivity {
                 2 = diamond
                 3 = heart
             */
-            String num;
+            int points;
 
             // set card
-            if (card_num == 11) {
-                num = "J";
-            } else if (card_num == 12) {
-                num = "Q";
-            } else if (card_num == 13) {
-                num = "K";
-            } else if (card_num == 1) {
-                num = "A";
-            } else {
-                num = card_num + "";
-            }
-
-            // set points
-            if (card_num > 10) {
-                card_num = 10;
+            if(card_num > 10) {
+                points = 10;
+            }else{
+                points = card_num;
             }
 
             // set cards on table
             if (place == 0) { // dealer card 1
-                textView = findViewById(R.id.dealer_c1);
-                textView.setText(num);
+                imageView = findViewById(R.id.dealer_ci1);
+                set_card_type(imageView,card_type,card_num);
 
                 // dealer total
                 textView = findViewById(R.id.dealer_total);
                 String d_total = textView.getText().toString().trim();
-                int d_total_int = Integer.parseInt(d_total) + card_num;
+                int d_total_int = Integer.parseInt(d_total) + points;
                 d_total = "" + d_total_int;
                 textView.setText(d_total);
             }  else if (place == 1) { // player card 1
-                textView = findViewById(R.id.player_c1);
-                textView.setText(num);
+                imageView = findViewById(R.id.player_ci1);
+                set_card_type(imageView,card_type,card_num);
 
                 // player total
                 textView = findViewById(R.id.player_total);
                 String p_total = textView.getText().toString().trim();
-                int p_total_int = Integer.parseInt(p_total) + card_num;
+                int p_total_int = Integer.parseInt(p_total) + points;
                 p_total = "" + p_total_int;
                 textView.setText(p_total);
             } else if (place == 2) { // player card 2
-                textView = findViewById(R.id.player_c2);
-                textView.setText(num);
+                imageView = findViewById(R.id.player_ci2);
+                set_card_type(imageView,card_type,card_num);
 
                 // player total
                 textView = findViewById(R.id.player_total);
                 String p_total = textView.getText().toString().trim();
-                int p_total_int = Integer.parseInt(p_total) + card_num;
+                int p_total_int = Integer.parseInt(p_total) + points;
                 p_total = "" + p_total_int;
                 textView.setText(p_total);
             }else if (place == 3) { // player optional card 3
-                textView = findViewById(R.id.player_c3);
-                textView.setText(num);
+                imageView = findViewById(R.id.player_ci3);
+                set_card_type(imageView,card_type,card_num);
 
                 // player total
                 textView = findViewById(R.id.player_total);
                 String p_total = textView.getText().toString().trim();
-                int p_total_int = Integer.parseInt(p_total) + card_num;
+                int p_total_int = Integer.parseInt(p_total) + points;
                 p_total = "" + p_total_int;
                 textView.setText(p_total);
             }else if (place == 4) { // dealer card 2
-                textView = findViewById(R.id.dealer_c2);
-                textView.setText(num);
+                imageView = findViewById(R.id.dealer_ci2);
+                set_card_type(imageView,card_type,card_num);
 
                 // dealer total
                 textView = findViewById(R.id.dealer_total);
                 String d_total = textView.getText().toString().trim();
-                int d_total_int = Integer.parseInt(d_total) + card_num;
+                int d_total_int = Integer.parseInt(d_total) + points;
                 d_total = "" + d_total_int;
                 textView.setText(d_total);
             } else { // if ERROR
@@ -313,6 +311,137 @@ public class PlayActivity extends AppCompatActivity {
             ).show();
         }// end if
     }// end winner
+
+    public void set_card_type(ImageView card, int type, int num){
+         /*
+                0 = spade
+                1 = club
+                2 = diamond
+                3 = heart
+
+                 1=Ace
+                    (need to figure out how to make A=(1 || 11))
+                2-10 = 2-10
+                11=Jack
+                12=Queen
+                13=King
+         */
+         if(type == 0){ // spades
+             if(num == 1){
+                 card.setImageResource(R.drawable.card_sa);
+             }else if(num == 2){
+                 card.setImageResource(R.drawable.card_s2);
+             }else if(num == 3){
+                 card.setImageResource(R.drawable.card_s3);
+             }else if(num == 4){
+                 card.setImageResource(R.drawable.card_s4);
+             }else if(num == 5){
+                 card.setImageResource(R.drawable.card_s5);
+             }else if(num == 6){
+                 card.setImageResource(R.drawable.card_s6);
+             }else if(num == 7){
+                 card.setImageResource(R.drawable.card_s7);
+             }else if(num == 8){
+                 card.setImageResource(R.drawable.card_s8);
+             }else if(num == 9){
+                 card.setImageResource(R.drawable.card_s9);
+             }else if(num == 10){
+                 card.setImageResource(R.drawable.card_s10);
+             }else if(num == 11){
+                 card.setImageResource(R.drawable.card_sj);
+             }else if(num == 12){
+                 card.setImageResource(R.drawable.card_sq);
+             }else if(num == 13){
+                 card.setImageResource(R.drawable.card_sk);
+             }
+         }else if (type == 1){ // clubs
+             if(num == 1){
+                 card.setImageResource(R.drawable.card_ca);
+             }else if(num == 2){
+                 card.setImageResource(R.drawable.card_c2);
+             }else if(num == 3){
+                 card.setImageResource(R.drawable.card_c3);
+             }else if(num == 4){
+                 card.setImageResource(R.drawable.card_c4);
+             }else if(num == 5){
+                 card.setImageResource(R.drawable.card_c5);
+             }else if(num == 6){
+                 card.setImageResource(R.drawable.card_c6);
+             }else if(num == 7){
+                 card.setImageResource(R.drawable.card_c7);
+             }else if(num == 8){
+                 card.setImageResource(R.drawable.card_c8);
+             }else if(num == 9){
+                 card.setImageResource(R.drawable.card_c9);
+             }else if(num == 10){
+                 card.setImageResource(R.drawable.card_c10);
+             }else if(num == 11){
+                 card.setImageResource(R.drawable.card_cj);
+             }else if(num == 12){
+                 card.setImageResource(R.drawable.card_cq);
+             }else if(num == 13){
+                 card.setImageResource(R.drawable.card_ck);
+             }
+         }else if(type == 2){ // diamonds
+             if(num == 1){
+                 card.setImageResource(R.drawable.card_da);
+             }else if(num == 2){
+                 card.setImageResource(R.drawable.card_d2);
+             }else if(num == 3){
+                 card.setImageResource(R.drawable.card_d3);
+             }else if(num == 4){
+                 card.setImageResource(R.drawable.card_d4);
+             }else if(num == 5){
+                 card.setImageResource(R.drawable.card_d5);
+             }else if(num == 6){
+                 card.setImageResource(R.drawable.card_d6);
+             }else if(num == 7){
+                 card.setImageResource(R.drawable.card_d7);
+             }else if(num == 8){
+                 card.setImageResource(R.drawable.card_d8);
+             }else if(num == 9){
+                 card.setImageResource(R.drawable.card_d9);
+             }else if(num == 10){
+                 card.setImageResource(R.drawable.card_d10);
+             }else if(num == 11){
+                 card.setImageResource(R.drawable.card_dj);
+             }else if(num == 12){
+                 card.setImageResource(R.drawable.card_dq);
+             }else if(num == 13){
+                 card.setImageResource(R.drawable.card_dk);
+             }
+         }else if(type == 3){ // hearts
+             if(num == 1){
+                 card.setImageResource(R.drawable.card_ha);
+             }else if(num == 2){
+                 card.setImageResource(R.drawable.card_h2);
+             }else if(num == 3){
+                 card.setImageResource(R.drawable.card_h3);
+             }else if(num == 4){
+                 card.setImageResource(R.drawable.card_h4);
+             }else if(num == 5){
+                 card.setImageResource(R.drawable.card_h5);
+             }else if(num == 6){
+                 card.setImageResource(R.drawable.card_h6);
+             }else if(num == 7){
+                 card.setImageResource(R.drawable.card_h7);
+             }else if(num == 8){
+                 card.setImageResource(R.drawable.card_h8);
+             }else if(num == 9){
+                 card.setImageResource(R.drawable.card_h9);
+             }else if(num == 10){
+                 card.setImageResource(R.drawable.card_h10);
+             }else if(num == 11){
+                 card.setImageResource(R.drawable.card_hj);
+             }else if(num == 12){
+                 card.setImageResource(R.drawable.card_hq);
+             }else if(num == 13){
+                 card.setImageResource(R.drawable.card_hk);
+             }
+         }else{ // ERROR
+
+         }
+    }// end setCardType
 
 }
 
